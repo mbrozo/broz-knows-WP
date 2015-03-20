d7a961be42a54e04c69d3b0d0a695742d56aa512
mbrozo <broz@brozknows.com>
2015-03-19 22:22:25 -0600

    Custom Post Types

--- wp-content/themes/broz_knows/functions.php
+++ wp-content/themes/broz_knows/functions.php
@@ -4,10 +4,15 @@
  *
  * @package broz-knows
  */
-
+if( function_exists('acf_add_options_page') ) {
+	
+	acf_add_options_page();
+	
+}
 /**
  * Set the content width based on the theme's design and stylesheet.
  */
+
 if ( ! isset( $content_width ) ) {
 	$content_width = 640; /* pixels */
 }
--- wp-content/themes/broz_knows/templates/homepage.php
+++ wp-content/themes/broz_knows/templates/homepage.php
@@ -14,76 +14,76 @@ get_header();
 </div>
 </div>
 
-<div class="drivers">
+<div class="services">
   <div class="row">
-    <!-- WEB DESIGN BOX -->
-    <div class="driver-column">
-      <img src="<?php the_field('image_driver_1');?>">
-      <h2><?php the_field('driver_title_1');?></h2>
-      <p><?php the_field('description_driver_1'); ?></p>
+    <!-- WEB DESIGN -->
+    <div class="column fourth services-column">
+      <a href=""><img class="services_image" src="<?php the_field('web_design_image');?>"></a>
+      <h2><?php the_field('web_design_title');?></h2>
+      <p><?php the_field('web_design_description'); ?></p>
     </div>
-    <!-- END WEB DESIGN BOX -->
+    <!-- END WEB DESIGN -->
 
-    <!-- WEB DESIGN BOX -->
-    <div class="driver-column">
-      <img src="<?php the_field('image_driver_2');?>">
-      <h2><?php the_field('title_driver_2');?></h2>
-      <p><?php the_field('description_driver_2'); ?></p>
+    <!-- APPAREL -->
+    <div class="column fourth services-column">
+      <a href=""><img class="services_image" src="<?php the_field('apparel_image');?>"></a>
+      <h2><?php the_field('apparel_title');?></h2>
+      <p><?php the_field('apparel_description'); ?></p>
     </div>
-    <!-- END WEB DESIGN BOX -->
+    <!-- END APPAREL -->
 
-    <!-- WEB DESIGN BOX -->
-    <div class="driver-column">
-      <img src="<?php the_field('image_driver_3');?>">
-      <h2><?php the_field('title_driver_3');?></h2>
-      <p><?php the_field('description_driver_3'); ?></p>
+    <!-- BRANDING -->
+    <div class="column fourth services-column">
+      <a href=""><img class="services_image" src="<?php the_field('branding_image');?>"></a>
+      <h2><?php the_field('branding_title');?></h2>
+      <p><?php the_field('branding_description'); ?></p>
     </div>
-    <!-- END WEB DESIGN BOX -->
+    <!-- END BRANDING -->
 
+    <!-- GRAPHIC DESIGN -->
+    <div class="column fourth services-column">
+      <a href=""><img class="services_image" src="<?php the_field('graphic_image');?>"></a>
+      <h2><?php the_field('graphic_title');?></h2>
+      <p><?php the_field('graphic_description'); ?></p>
+    </div>
+    <!-- GRAPHIC DESIGN -->
   </div>
 </div>
 
+<!-- CUSTOM POST TYPE -->
+<div class="row">
+<?php
+    // The Arguments
+    $args = array(
+        'post_type' => 'portfolio',  // Name of Your Custom Post Type
+        'posts_per_page' => 3       // Number of Posts to Retrieve
+    );
+    // Start Loop
+    $loop = new WP_Query( $args );
+    while ( $loop->have_posts() ) : $loop->the_post();
+?>
 
-
-
-<!--  
-
-<div class="row clearfix">
-  <div class="column_home_portfolio">
-    <img class="portfolio_image" src="<?php bloginfo('template_directory'); ?>/images/home_web.jpg"/>
-    <h2 class="h1_portfolio">Web Design</h2>
-    <p>Broz's belief is that web design should be accessible for every individual and business; not just large companies with large budgets. Since its founding in 2001, Broz has become a stable, well-respected design firm that offers aff
-    <p><b>To learn more about Broz's web design & development services, click here.</b></p>
-  </div>
-
-  <div class="column_home_portfolio">
-    <img class="portfolio_image" src="/<?php bloginfo('template_directory'); ?>/images/home_apparel.jpg"/>
-    <h2 class="h1_portfolio">Apparel & Swag</h2>
-    <p>Broz is your one-stop shop for custom design t-shirts, apparel and marketing & promotional items. Broz will design the artwork or logo for your project, offer you options from a wide variety of wholesale vendors and faciitate press checks and printing.</p>
-    <p><b>Click here to learn more about Broz's apparel and swag offerings.</b></p>
-  </div>
- 
-  <div class="column_home_portfolio">
-    <img class="portfolio_image" src="<?php bloginfo('template_directory'); ?>/images/home_branding.jpg"/>
-    <h2 class="h1_portfolio">Branding</h2>
-    <p>Branding goes far beyond just having a great logo; it's an all-encompassing concept that applies to advertising, signage, business cards, letterhead, an online presence and much, much more.</p>
-    <p><b>For more on branding or to see Broz's branding portfolio, click here.</b></p>
-  </div>
-  
-  <div class="column_home_portfolio">
-    <img class="portfolio_image" src="<?php bloginfo('template_directory'); ?>/images/home_graphic.jpg"/>
-    <h2 class="h1_portfolio">Graphic Design</h2>
-    <p>Broz has extensive experience in graphic design including custom illustration, logo development, marketing collateral, advertising, signage, packaging, menu design and invitations.</p>
-  </div>
+<div class="column third">
+  <a class="featured-image-overlay" href="<?php the_permalink(); ?>"><img src="<?php the_field('portfolio_feature_image'); ?>" /></a>
+  <a href="<?php the_permalink(); ?>"><h3><?php the_title(); ?></h3></a>
+  <p><?php the_field('portfolio_short_description'); ?></p>
 </div>
 
--->
+<?php
+    // Resets the Loop
+    endwhile;
+    wp_reset_postdata();
+?>
+</div>
+<!-- CUSTOM POST TYPE ENDS -->
+
+
 
 <!-- Broz Box Starts Here -->
 <div class="broz_box clearfix">
   <div class="row">
     <h2 class="h1_broz_box">Whom or What is Broz?</h2>
-    <p>My grandfather once said that no man should give himself his own nickname. Our Eastern Bloc handle Brozovich trips over most people's tongues and has seen its share of shortenings, mispronunciations and misspellings. Through the years, the only common thing regarding our last name was the shortening of it to Broz (that, and sharing the shortened name with a <a href="http://en.wikipedia.org/wiki/Josip_Broz_Tito" target="_blank">former most benevolent dictator of Yugoslavia</a>). I have been signing my artwork with "Broz" for as long as I can remember, so it seemed like a natural progression to honor that tradition when naming my design firm.</p>
+    <p><?php the_field('footer_blurb', 'options'); ?></p>
     <img class="broz_head" src="<?php bloginfo('template_directory'); ?>/images/whoisbroz.png" alt="Broz Knows"/>
   </div>
 </div>
